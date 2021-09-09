6d3cb558432c4b9e3badf2d39eb34aeb837e0aa0
Ramid Khan <ramidzkh@gmail.com>
2021-09-09 21:28:25 +1000

    System property to use alternate hashing

--- src/main/java/me/hydos/rosella/util/HashUtil.java
+++ src/main/java/me/hydos/rosella/util/HashUtil.java
@@ -6,7 +6,14 @@ import java.nio.ByteBuffer;
 
 public class HashUtil {
 
+    private static final boolean USE_XXHASH_WITH_SEED = Boolean.parseBoolean(System.getProperty("rosella:xxhash_with_seed"));
+    private static final int SEED = 0xC7AB;
+
     public static long hash64(ByteBuffer buffer) {
-        return XXHash.XXH3_64bits(buffer);
+        if (USE_XXHASH_WITH_SEED) {
+            return XXHash.XXH64(buffer, SEED);
+        } else {
+            return XXHash.XXH3_64bits(buffer);
+        }
     }
 }
