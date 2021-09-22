2ea82de22c02c852c0a832642bdc9e78e3aebafc
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2021-09-22 10:27:52 +0200

    Added ListSpacePartition

---
+++ src/main/java/graphics/kiln/rosella/util/ListSpacePartition.java
@@ -0,0 +1,127 @@
+package graphics.kiln.rosella.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BiConsumer;
+
+public class ListSpacePartition<T> {
+
+    private final int dimensionCount;
+
+    private Partition partitionList = null;
+
+    public ListSpacePartition(int dimensionCount) {
+        this.dimensionCount = dimensionCount;
+    }
+
+    public void insert(@Nullable T newState, @NotNull int[] regionStart, @NotNull int[] regionEnd, @Nullable BiConsumer<Partition, T> transitionFunction) {
+        if(this.partitionList == null) {
+            this.partitionList = new Partition(newState, regionStart, regionEnd);
+        } else {
+            this.partitionList.insert(null, newState, regionStart, regionEnd, transitionFunction);
+        }
+    }
+
+    public class Partition {
+        private Partition next = null;
+
+        private final T state;
+
+        private final int[] start;
+        private final int[] end;
+
+        protected Partition(T state, int[] start, int[] end) {
+            this.state = state;
+            this.start = start;
+            this.end = end;
+        }
+
+        protected Partition(T state, int[] start, int[] end, Partition next) {
+            this.next = next;
+
+            this.state = state;
+            this.start = start;
+            this.end = end;
+        }
+
+        public boolean intersects(int[] otherStart, int[] otherEnd) {
+            for(int i = 0; i < dimensionCount; i++) {
+                if(this.start[i] >= otherEnd[i] || this.end[i] < otherStart[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        protected void insert(Partition previous, T otherState, int[] otherStart, int[] otherEnd, BiConsumer<Partition, T> transitionFunction) {
+            if(this.intersects(otherStart, otherEnd)) {
+                for(int i = 0; i < dimensionCount; i++) {
+                    if(otherStart[i] > this.start[i]) {
+                        int[] splitEnd = this.end.clone();
+                        splitEnd[i] = otherStart[i];
+
+                        Partition split = new Partition(this.state, this.start.clone(), splitEnd, this);
+                        if(previous != null) {
+                            previous.next = split;
+                        } else {
+                            partitionList = split;
+                        }
+                        previous = split;
+
+                        this.start[i] = otherStart[i];
+                    }
+                    if(otherEnd[i] < this.end[i]) {
+                        int[] splitStart = this.start.clone();
+                        splitStart[i] = otherEnd[i];
+
+                        Partition split = new Partition(this.state, splitStart, this.end.clone(), this);
+                        if(previous != null) {
+                            previous.next = split;
+                        } else {
+                            partitionList = split;
+                        }
+                        previous = split;
+
+                        this.end[i] = otherEnd[i];
+                    }
+                }
+                previous.next = next;
+
+                if(transitionFunction != null) {
+                    transitionFunction.accept(this, otherState);
+                }
+            } else if((this.state == null && otherState == null) || (this.state != null && this.state.equals(otherState))){
+                // Test if merger is possible
+                boolean merge = false;
+                for(int i = 0; i < dimensionCount && !merge; i++) {
+                    if(otherEnd[i] == this.start[i] || otherStart[i] == this.end[i]) {
+                        merge = true;
+                        for(int j = 0; j < dimensionCount; j++) {
+                            if(i == j) {
+                                continue;
+                            }
+                            if(this.start[j] != otherStart[j] || this.end[j] != otherEnd[j]) {
+                                merge = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if(merge) {
+                    for(int i = 0; i < dimensionCount; i++) {
+                        this.start[i] = Math.min(this.start[i], otherStart[i]);
+                        this.end[i] = Math.max(this.end[i], otherEnd[i]);
+                    }
+                    return;
+                }
+            }
+
+            if(this.next != null) {
+                this.next.insert(this, otherState, otherStart, otherEnd, transitionFunction);
+            } else {
+                this.next = new Partition(otherState, otherStart, otherEnd);
+            }
+        }
+    }
+}
